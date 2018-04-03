ff2940486b313e859bc1056e058098f3f484d3fa
unknown <274736996@qq.com>
2018-04-04 00:26:36 +0800

    parameterized test

--- src/test/TestCountNumber.java
+++ src/test/TestCountNumber.java
@@ -3,20 +3,45 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import sourceCode.CountNumber;
 
-class TestCountNumber {
-	CountNumber aCount = new CountNumber();
 
-	@Test
-	void testCount() {
-//		fail("ипн╢й╣ож");
-		System.out.println("test count\n");
-	
-		String exp1 = aCount.readFile("shouldBe1.txt");
-		assertEquals("count wrong",exp1,aCount.count("text1.txt").toString());
+@RunWith (Parameterized.class)
+public class TestCountNumber {
+	private String expPath;
+	private String sourcePath;
+	private CountNumber aCount;
+ 
+	public TestCountNumber(String expPath,String sourcePath) {
+		this.expPath = expPath;
+		this.sourcePath = sourcePath;
 	}
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] object = {{"TCofCN/shouldBe1.txt","TCofCN/text1.txt"}
+							,{"TCofCN/shouldBe2.txt","TCofCN/text2.txt"}
+							};
+		return Arrays.asList(object);
+	 
+	}
+	
+	@Test
+	public void testCount() {
+		this.aCount = new CountNumber();
+		String exp = aCount.readFile(expPath);
+		assertEquals(exp, aCount.count(this.sourcePath).toString() );
+	}
+	
+
 
 }
