494e82509a5dd001e7d12aeab1756fc3244c8c34
unknown <274736996@qq.com>
2018-04-05 21:19:27 +0800

    add 20 test cases

--- src/test/TestCountNumber.java
+++ src/test/TestCountNumber.java
@@ -28,8 +28,27 @@ public class TestCountNumber {
 	
 	@Parameters
 	public static Collection<Object[]> data() {
-		Object[][] object = {{"TCofCN/shouldBe1.txt","TCofCN/text1.txt"}
+		Object[][] object = {
+							 {"TCofCN/shouldBe1.txt","TCofCN/text1.txt"}
 							,{"TCofCN/shouldBe2.txt","TCofCN/text2.txt"}
+							,{"TCofCN/shouldBe3.txt","TCofCN/text3.txt"}
+							,{"TCofCN/shouldBe4.txt","TCofCN/text4.txt"}
+							,{"TCofCN/shouldBe5.txt","TCofCN/text5.txt"}
+							,{"TCofCN/shouldBe6.txt","TCofCN/text6.txt"}
+							,{"TCofCN/shouldBe7.txt","TCofCN/text7.txt"}
+							,{"TCofCN/shouldBe8.txt","TCofCN/text8.txt"}
+							,{"TCofCN/shouldBe9.txt","TCofCN/text9.txt"}
+							,{"TCofCN/shouldBe10.txt","TCofCN/text10.txt"}
+							,{"TCofCN/shouldBe11.txt","TCofCN/text11.txt"}
+							,{"TCofCN/shouldBe12.txt","TCofCN/text12.txt"}
+							,{"TCofCN/shouldBe13.txt","TCofCN/text13.txt"}
+							,{"TCofCN/shouldBe14.txt","TCofCN/text14.txt"}
+							,{"TCofCN/shouldBe15.txt","TCofCN/text15.txt"}
+							,{"TCofCN/shouldBe16.txt","TCofCN/text16.txt"}
+							,{"TCofCN/shouldBe17.txt","TCofCN/text17.txt"}
+							,{"TCofCN/shouldBe18.txt","TCofCN/text18.txt"}
+							,{"TCofCN/shouldBe19.txt","TCofCN/text19.txt"}
+							,{"TCofCN/shouldBe20.txt","TCofCN/text20.txt"}
 							};
 		return Arrays.asList(object);
 	 
